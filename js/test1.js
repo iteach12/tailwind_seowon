80de47c90d5f0d0177a38587a866cfdd4a874a01
iteach12 <iteach12@gmail.com>
2021-01-30 09:12:27 +0900

    Update test1.js

--- js/test1.js
+++ js/test1.js
@@ -1,5 +1,10 @@
-var myName = prompt("이름을 입력해 주세요.");
+var age = prompt("나이를 입력해 주세요.");
 
-alert(`${myName}님, 안녕하세요?`);
+if(age >= 18){
+    alert(`${age}은/는 성인입니다.`);
+}
+else{
+    document.write(`${age}은/는 청소년입니다.`);
+}
 
-document.write(myName);
+(age >= 20)?alert(`성인입니다.`):alert(`미성년입니다.`);
